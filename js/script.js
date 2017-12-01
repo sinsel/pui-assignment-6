62a4caa4782991d59cee3c6eba93f1f9bf91bc3d
Selin Insel <sinsel@andrew.cmu.edu>
2017-11-30 22:00:08 -0500

    final update

--- js/script.js
+++ js/script.js
@@ -12,53 +12,53 @@ $(function() {
 		});
 
 		// Spinning Animation
-	$('#skate-spin').mouseenter(
-		function() {
-			$('#skate-spin').addClass('animated flip').one(animationEndWords , function() {
-				$(this).removeClass('animated flip')
+		$('#skate-spin').mouseenter(
+			function() {
+				$('#skate-spin').addClass('animated flip').one(animationEndWords , function() {
+					$(this).removeClass('animated flip')
+				});
 			});
-		});
 
-// Spin Page Animations
-	$('#scratch-spin').mouseenter(
-		function() {
-			$('#scratch-spin').addClass('animated flip').one(animationEndWords , function() {
-				$(this).removeClass('animated flip')
-			});
-		});
+			// Spin Page Animations
+			$('#scratch-spin').mouseenter(
+				function() {
+					$('#scratch-spin').addClass('animated flip').one(animationEndWords , function() {
+						$(this).removeClass('animated flip')
+					});
+				});
 
-	$('#layback-spin').mouseenter(
-		function() {
-			$('#layback-spin').addClass('animated flip').one(animationEndWords , function() {
-				$(this).removeClass('animated flip')
-			});
-		});
+				$('#layback-spin').mouseenter(
+					function() {
+						$('#layback-spin').addClass('animated flip').one(animationEndWords , function() {
+							$(this).removeClass('animated flip')
+						});
+					});
 
-	$('#biellman-spin').mouseenter(
-		function() {
-			$('#biellman-spin').addClass('animated flip').one(animationEndWords , function() {
-				$(this).removeClass('animated flip')
-			});
-		});
+					$('#biellman-spin').mouseenter(
+						function() {
+							$('#biellman-spin').addClass('animated flip').one(animationEndWords , function() {
+								$(this).removeClass('animated flip')
+							});
+						});
 
-	$('#camel-spin').mouseenter(
-		function() {
-			$('#camel-spin').addClass('animated flip').one(animationEndWords , function() {
-				$(this).removeClass('animated flip')
-			});
-		});
+						$('#camel-spin').mouseenter(
+							function() {
+								$('#camel-spin').addClass('animated flip').one(animationEndWords , function() {
+									$(this).removeClass('animated flip')
+								});
+							});
 
-	$('#doughnut-spin').mouseenter(
-		function() {
-			$('#doughnut-spin').addClass('animated flip').one(animationEndWords , function() {
-				$(this).removeClass('animated flip')
-			});
-		});
+							$('#doughnut-spin').mouseenter(
+								function() {
+									$('#doughnut-spin').addClass('animated flip').one(animationEndWords , function() {
+										$(this).removeClass('animated flip')
+									});
+								});
 
-	$('#sit-spin').mouseenter(
-		function() {
-			$('#sit-spin').addClass('animated flip').one(animationEndWords , function() {
-				$(this).removeClass('animated flip')
-			});
-		});
-});
+								$('#sit-spin').mouseenter(
+									function() {
+										$('#sit-spin').addClass('animated flip').one(animationEndWords , function() {
+											$(this).removeClass('animated flip')
+										});
+									});
+								});
